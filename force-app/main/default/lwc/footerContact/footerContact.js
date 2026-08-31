a21245f38fd3c00e8c9fe7f98e052d5da4b8a6ac
Jean Nieto <jeann@epidataconsulting.com>
2026-08-31 14:49:13 -0400

    SE-116 componnetes cta

--- force-app/main/default/lwc/footerContact/footerContact.js
+++ force-app/main/default/lwc/footerContact/footerContact.js
@@ -1,13 +1,33 @@
-import { LightningElement } from 'lwc';
-import logows from '@salesforce/resourceUrl/logows';
+import { LightningElement, wire } from "lwc";
+import logows from "@salesforce/resourceUrl/logows";
+import getFooterCtaConfig from "@salesforce/apex/FooterContactController.getFooterCtaConfig";
 
 export default class FooterContact extends LightningElement {
-    iconContactenosUrl = logows;
+  iconContactenosUrl = logows;
 
-    handleWhatsapp() {
-        const phone = '5491131172022'; // Número con código de país, sin signos
-        const message = 'Hola, quiero más información';
-        const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-        window.open(url, '_blank');
+  mostrarCta = false;
+  textoCta = "¡Resolvé tu duda!";
+  urlCta = "";
+
+  @wire(getFooterCtaConfig)
+  wiredConfig({ error, data }) {
+    if (data) {
+      this.mostrarCta = data.mostrar;
+      this.textoCta = data.texto;
+      this.urlCta = data.url;
+    } else if (error) {
+      console.error(error);
     }
+  }
+
+  handleWhatsapp() {
+    const phone = "5491131172022"; // Número con código de país, sin signos
+    const message = "Hola, quiero más información";
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+  }
+
+  handleResolveDuda() {
+    window.open(this.urlCta, "_blank");
+  }
 }
