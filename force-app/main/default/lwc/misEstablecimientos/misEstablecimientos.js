86747ca99637b4ce006200f213b7b879b4c75c62
Alexander CR <alexandercr@epidataconsulting.com.test>
2026-09-01 10:04:23 -0500

    Fix misEstablecimientos api import for LWC compiler.

--- force-app/main/default/lwc/misEstablecimientos/misEstablecimientos.js
+++ force-app/main/default/lwc/misEstablecimientos/misEstablecimientos.js
@@ -1,4 +1,4 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import getEstablecimientos from '@salesforce/apex/misEstablecimientosController.getEstablecimientos';
 import { doRequest } from 'c/utils';
 
